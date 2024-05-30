b7382f73d739b9cc3994fc73c7ed876ee9cda7ba
DTamm <tamtdgcs200734@fpt.edu.vn>
2024-05-30 13:33:53 +0700

    new

--- sever.js
+++ sever.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-res.send('Hello, World!')
+res.send('Hello, World abcd!')
 })
-app.listen(3000, () => {
+app.listen(3005, () => {
 console.log('Server started on port 3000')
 })
